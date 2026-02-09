377158a3de66877a871a014b9e23355653133805
pcm <filsamare@gmail.com>
2026-02-09 20:32:45 +0700

    Fresh start

--- build.bat
+++ build.bat
@@ -4,9 +4,9 @@
 if not exist build mkdir build
 
 set CompileFlags=%CompileFlags% /nologo /Zi /FC /std:c11 /GS- /Gs999999 /Od /Oi /W4 /WX /wd4101 /wd4189 /wd4100
-set LinkFlags=/incremental:no /opt:icf /opt:ref /nodefaultlib /subsystem:efi_application /entry:EntryUEFI
+set LinkFlags=/incremental:no /opt:icf /opt:ref /nodefaultlib /subsystem:efi_application /entry:UEFIBoot
 
 pushd build
-call cl %CompileFlags% /Fe:BOOTX64.efi "..\code\uefi_pone.c" /link %LinkFlags% %UEFILinkFlags%
+call cl %CompileFlags% /Fe:BOOTX64.efi "..\code\uefi.c" /link %LinkFlags% %UEFILinkFlags%
 call ..\tools\write_gpt -ds 29 -i pone.img
 popd
---
+++ code/uefi.c
@@ -0,0 +1,10 @@
+
+#include "shared.h"
+#include "shared.c"
+
+#include "uefi.h"
+
+efi_status EFI_API UEFIBoot(efi_handle ImageHandle, efi_system_table* SystemTable)
+{
+    return (EFI_SUCCESS);
+}
